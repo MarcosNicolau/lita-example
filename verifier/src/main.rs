8a36c664e51d7d9b742bdb600837b18ee8fa2823
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2025-06-17 15:41:20 -0300

    feat: parallel verification

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -1,23 +1,43 @@
-use std::path::Path;
+use std::{path::Path, sync::LazyLock, thread};
 #[cfg(target_arch = "aarch64")]
 use valida_vm_api_linux_arm::*;
 #[cfg(target_arch = "x86_64")]
 use valida_vm_api_linux_x86::*;
+
+static VALIDA: LazyLock<Valida> = LazyLock::new(|| create_valida().unwrap());
 
 fn main() {
     let program = Path::new("test_data").join("program.bin");
     let proof = Path::new("test_data").join("proof.bin");
     let stdout = Path::new("test_data").join("stdout");
 
-    let valida = create_valida().unwrap();
-    let verify_status_correct_statement = valida.verify(
-        &program,
-        proof.as_ref(),
-        &stdout.as_ref(),
-        Default::default(),
-        Default::default(),
-    );
-    assert_eq!(verify_status_correct_statement, VerifyStatus::Success);
+    let num_threads = std::thread::available_parallelism()
+        .map(|n| n.get())
+        .unwrap_or(1);
+
+    let handles: Vec<_> = (0..num_threads)
+        .map(|i| {
+            let program = program.clone();
+            let proof = proof.clone();
+            let stdout = stdout.clone();
+
+            thread::spawn(move || {
+                let status = VALIDA.verify(
+                    &program,
+                    &proof,
+                    &stdout,
+                    Default::default(),
+                    Default::default(),
+                );
+                assert_eq!(status, VerifyStatus::Success, "Thread {i} failed");
+                println!("Thread {i} verification successful");
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().expect("Thread panicked");
+    }
 
     println!("All checks completed successfully.");
 }
